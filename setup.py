c9293564b527c14fd482e2670da998580f6d1c71
Subham <subham.kapoor@happay.in>
2016-09-17 13:13:24 +0530

    Fixes

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 setup(
   name = 'QuerysetLimitor',
   packages = ['QuerysetLimitor'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A',
+  version = '1.0',
+  description = 'A queryset wrapper to ',
   author = 'Subham Kapoor',
   author_email = 'subhamkapoor360@gmail.com',
   url = 'https://github.com/subhamkapoor360/HappaySDK', # use the URL to the github repo
