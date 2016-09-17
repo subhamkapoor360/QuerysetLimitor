50440115e26ca4cc4c6b9390c42abc742c528b48
Subham <subham.kapoor@happay.in>
2016-09-17 12:58:17 +0530

    Done

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'QuerysetLimitor',
+  packages = ['QuerysetLimitor'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A',
+  author = 'Subham Kapoor',
+  author_email = 'subhamkapoor360@gmail.com',
+  url = 'https://github.com/subhamkapoor360/HappaySDK', # use the URL to the github repo
+  download_url = 'https://github.com/subhamkapoor360/HappaySDK/tarball/0.2', # I'll explain this in a second
+  keywords = ['Django','QuerySet','QuerysetLimitor'], # arbitrary keywords
+  classifiers = [],
+)
